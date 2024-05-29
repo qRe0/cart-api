788a7e8c0cd90ab7d1142a0966ea58c0361a906c
qRe0 <melsenmalina@gmail.com>
2024-05-29 20:45:17 +0300

    style: make new messages for errors

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -4,10 +4,10 @@ import "errors"
 
 var (
 	ErrCreatingCart      = errors.New("error creating cart")
-	ErrWrongCartID       = errors.New("error with cart id (wrong cart id)")
-	ErrWrongItemID       = errors.New("error with item id (wrong item id)")
-	ErrCartNotFound      = errors.New("error finding cart id in database (cart not found)")
-	ErrItemNotFound      = errors.New("error finding item id in database (item not found)")
+	ErrWrongCartID       = errors.New("error wrong cart id")
+	ErrWrongItemID       = errors.New("error wrong item id")
+	ErrCartNotFound      = errors.New("error cart not found")
+	ErrItemNotFound      = errors.New("error item not found")
 	ErrEmptyProductName  = errors.New("error empty product name")
 	ErrWrongItemQuantity = errors.New("error wrong item quantity")
 	ErrClosingDB         = errors.New("error closing database")
