eedb63f3fe8e05785af8ec1ca782901f3cb49360
qRe0 <melsenmalina@gmail.com>
2024-05-29 20:34:01 +0300

    refactor: remove unused error

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -3,13 +3,12 @@ package errors
 import "errors"
 
 var (
-	ErrCreatingCart       = errors.New("error creating cart")
-	ErrWrongCartID        = errors.New("error with cart id (wrong cart id)")
-	ErrWrongItemID        = errors.New("error with item id (wrong item id)")
-	ErrCartNotFound       = errors.New("error finding cart id in database (cart not found)")
-	ErrItemNotFound       = errors.New("error finding item id in database (item not found)")
-	ErrRemoveItemFromCart = errors.New("error removing item from cart")
-	ErrEmptyProductName   = errors.New("error empty product name")
-	ErrWrongItemQuantity  = errors.New("error wrong item quantity")
-	ErrClosingDB          = errors.New("error closing database")
+	ErrCreatingCart      = errors.New("error creating cart")
+	ErrWrongCartID       = errors.New("error with cart id (wrong cart id)")
+	ErrWrongItemID       = errors.New("error with item id (wrong item id)")
+	ErrCartNotFound      = errors.New("error finding cart id in database (cart not found)")
+	ErrItemNotFound      = errors.New("error finding item id in database (item not found)")
+	ErrEmptyProductName  = errors.New("error empty product name")
+	ErrWrongItemQuantity = errors.New("error wrong item quantity")
+	ErrClosingDB         = errors.New("error closing database")
 )
