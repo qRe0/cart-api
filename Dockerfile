c28316f157222e2c87f72e3da0671f07015397a0
qRe0 <melsenmalina@gmail.com>
2024-05-30 13:13:55 +0300

    feat: create migrations

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,7 @@ RUN go mod download
 COPY . .
 
 RUN go build -o start ./cmd/main.go
+RUN go install github.com/pressly/goose/v3/cmd/goose@latest
 
 FROM alpine:latest
 
@@ -17,7 +18,9 @@ WORKDIR /root/
 
 COPY --from=builder /app/start .
 COPY --from=builder /app/.env .
+COPY --from=builder /go/bin/goose /usr/local/bin/goose
+COPY internal/migrations /migrations
 
 EXPOSE ${API_PORT}
 
-CMD ["./start"]
+CMD ["/bin/sh", "-c", "goose -dir /migrations postgres \"postgres://${DATABASE_USER}:${DATABASE_PASSWORD}@${DATABASE_HOST}:${DB_PORT}/${DATABASE_NAME}?sslmode=disable\" up && ./start"]
---
+++ internal/migrations/20240530093854_cart_db.sql
@@ -0,0 +1,24 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE IF NOT EXISTS "carts"
+(
+    "id" SERIAL PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS "items"
+(
+    "id"       SERIAL PRIMARY KEY,
+    "cart_id"  INTEGER NOT NULL,
+    "product"  TEXT    NOT NULL,
+    "quantity" INTEGER NOT NULL,
+    UNIQUE (cart_id, product),
+    CONSTRAINT "items_cart_id_foreign" FOREIGN KEY ("cart_id") REFERENCES "carts" ("id") ON DELETE CASCADE
+);
+
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE IF EXISTS "items";
+DROP TABLE IF EXISTS "carts";
+-- +goose StatementEnd
