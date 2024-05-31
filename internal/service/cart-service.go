0a6f9c6164992798be24afabea8346a4f0121441
qRe0 <melsenmalina@gmail.com>
2024-06-01 00:00:23 +0300

    refactor: change err handling logic

--- internal/service/cart-service.go
+++ internal/service/cart-service.go
@@ -1,8 +1,11 @@
 package service
 
 import (
+	"errors"
+	"fmt"
 	"strconv"
 
+	"github.com/lib/pq"
 	errs "github.com/qRe0/innowise-cart-api/internal/errors"
 	"github.com/qRe0/innowise-cart-api/internal/models"
 	"github.com/qRe0/innowise-cart-api/internal/repository"
@@ -40,15 +43,17 @@ func (c *CartService) AddItemToCart(cartIDStr string, item models.CartItem) (*mo
 		return nil, errs.ErrWrongItemQuantity
 	}
 
-	cartExists, err := c.repo.IsCartExist(cartID)
-	if !cartExists || err != nil {
-		return nil, errs.ErrCartNotFound
-	}
-
 	item.CartID = cartID
 	result, err := c.repo.AddItemToCart(item)
 	if err != nil {
-		return nil, errs.ErrCartNotFound
+		var pqErr *pq.Error
+		if errors.As(err, &pqErr) {
+			if pqErr.Code == "23503" {
+				return nil, errs.ErrCartNotFound
+			} else {
+				return nil, fmt.Errorf("database error: %s", pqErr.Message)
+			}
+		}
 	}
 
 	return result, nil
