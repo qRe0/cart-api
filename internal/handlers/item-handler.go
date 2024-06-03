8a69b7785d8aa4756f0a2369a4b0800611610493
qRe0 <melsenmalina@gmail.com>
2024-06-03 13:00:23 +0300

    refactor: add resp header

--- internal/handlers/item-handler.go
+++ internal/handlers/item-handler.go
@@ -65,6 +65,7 @@ func (h *ItemHandler) RemoveItemFromCart(w http.ResponseWriter, r *http.Request)
 	}
 
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 	_, err = w.Write([]byte("{}"))
 	if err != nil {
 		http.Error(w, "error encoding", http.StatusBadRequest)
