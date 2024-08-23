d22c89bd37a4d3406384be175944e2a36e2e6faa
qRe0 <melsenmalina@gmail.com>
2024-08-23 13:22:17 +0200

    Change errors messages

--- internal/errors/errors.go
+++ internal/errors/errors.go
@@ -3,7 +3,7 @@ package errors
 import "errors"
 
 var (
-	ErrCreateCart        = errors.New("error can't create cart")
+	ErrCreateCart        = errors.New("error creating cart")
 	ErrWrongCartID       = errors.New("error wrong cart id")
 	ErrWrongItemID       = errors.New("error wrong item id")
 	ErrCartNotFound      = errors.New("error cart not found")
@@ -12,7 +12,6 @@ var (
 	ErrWrongItemQuantity = errors.New("error wrong item quantity")
 	ErrClosingDB         = errors.New("error closing database")
 	ErrLoadEnvVars       = errors.New("error loading environment variables")
-	ErrEncoding          = errors.New("error encoding")
 	ErrDecodingReqBody   = errors.New("error decoding request body")
 	ErrStartTransaction  = errors.New("error starting transaction")
 	ErrCommitTransaction = errors.New("error committing transaction")
