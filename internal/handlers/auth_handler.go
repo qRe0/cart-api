fa000e7d5abb1471fc402a0efe8aff5c766593d4
qRe0 <melsenmalina@gmail.com>
2024-08-27 17:56:42 +0200

    Implemented Refresh endpoint (release new access and refresh tokens)

--- internal/handlers/auth_handler.go
+++ internal/handlers/auth_handler.go
@@ -113,6 +113,49 @@ func (h *AuthHandler) LogOut(c *gin.Context) {
 	// #TODO: Implement LogOut
 }
 
+// Refresh godoc
+// @Tags Public routes. Registration and Authentication
+// @Summary Refresh user tokens (Access and Refresh)
+// @Schemes
+// @Description This method allows user to release new access and refresh tokens
+// @Accept json
+// @Produce json
+// @Param token body models.RefreshRequest true "Input data for token refresh"
+// @Success 200 {object} models.RefreshResponse "Tokens refreshed successfully"
+// @Failure 400 {object} models.ErrorResponse "Invalid input data"
+// @Failure 401 {object} models.ErrorResponse "Unauthorized"
+// @Failure 500 {object} models.ErrorResponse "Internal server error"
+// @Router /auth/refresh [post]
 func (h *AuthHandler) Refresh(c *gin.Context) {
-	// #TODO: Implement Refresh
+	var token pb.RefreshRequest
+	err := c.ShouldBindJSON(&token)
+	if err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	log.Println(token.RefreshToken)
+
+	md := metadata.MD{}
+	ctx := metadata.NewOutgoingContext(c.Request.Context(), md)
+
+	var metadataHeader metadata.MD
+	log.Println(token.RefreshToken)
+	resp, err := h.refreshClient.Refresh(ctx, &token, grpc.Header(&metadataHeader))
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	newAccessToken := metadataHeader.Get("Authorization")
+	newRefreshToken := metadataHeader.Get("Refresh-Token")
+	if len(newAccessToken) == 0 || len(newRefreshToken) == 0 {
+		c.JSON(401, gin.H{"error": "Unauthorized"})
+		return
+	}
+
+	c.Header("Authorization", newAccessToken[0])
+	c.Header("Refresh-Token", newRefreshToken[0])
+
+	c.JSON(200, gin.H{"message": resp.Message})
 }
